bb7f169ca56d04ee3e7f4511479b97316f94dd23
Alabhya Pandey <alabhyap@gmail.com>
2019-01-14 13:03:31 +0530

    Added javaDoc Documentation

--- app/src/main/java/com/alabhya/Shaktiman/VendorSignupActivity.java
+++ app/src/main/java/com/alabhya/Shaktiman/VendorSignupActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -47,7 +46,10 @@ public class VendorSignupActivity extends AppCompatActivity {
     private List<String> StateName = new ArrayList<>();
     private int citySize, localitySize;
 
-    private EditText fullName,password, mobileNumber, aadhar;
+    private EditText fullName;
+    private EditText password;
+    private EditText mobileNumber;
+    private EditText aadhar;
     private TextView dob;
     NiceSpinner spinnerCity, spinnerState, spinnerLocality;
 
@@ -69,8 +71,8 @@ public class VendorSignupActivity extends AppCompatActivity {
 
 
         /*   **************************
-         ***TOOLBAR SECTION CODE***
-         **************************
+             ***TOOLBAR SECTION CODE***
+             **************************
          */
         setSupportActionBar(toolbar);
 
@@ -91,8 +93,8 @@ public class VendorSignupActivity extends AppCompatActivity {
         toolbar.setTitle("Producer SignUp");
 
         /*   **************************
-         ***APICALL SECTION CODE***
-         **************************
+             ***APICALL SECTION CODE***
+             **************************
          */
 
         apiInterface = ApiClient.getRetrofitClient().create(ApiInterface.class);
@@ -108,32 +110,12 @@ public class VendorSignupActivity extends AppCompatActivity {
         StateName.add("Uttar Pradesh");
         spinnerState.attachDataSource(StateName);
 
-        /*   **************************
-         ***  DOB SECTION CODE  ***
-         **************************
+        /*  **************************
+            ***  DOB SECTION CODE  ***
+            **************************
          */
         dob = findViewById(R.id.dateOfBirth);
-        dob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) throws NullPointerException {
-                Calendar calendar = Calendar.getInstance();
-                int year = calendar.get(Calendar.YEAR);
-                int month = calendar.get(Calendar.MONTH);
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-                Log.d("Single",""+spinnerLocality.getSelectedIndex());
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        VendorSignupActivity.this,
-                        android.R.style.Theme_Material_Light_Dialog_NoActionBar_MinWidth,
-                        onDateSetListener,
-                        year, month, day
-                );
-
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
-                dialog.show();
-            }
-        });
+        dob.setOnClickListener(dateOfBirthListener);
 
         //Update date using the dialog of Calendar Instance
         onDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -155,102 +137,62 @@ public class VendorSignupActivity extends AppCompatActivity {
             }
         };
 
-        /*   **************************
-             ***SiGNUP SECTION CODE ***
-             **************************
+        /*
+         * Function call for Signup on Button Click
          */
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*TokenRequest tokenRequest = new TokenRequest();
-                tokenRequest.setFullName(fullName.getText().toString().trim());
-                tokenRequest.setPassword(password.getText().toString().trim());
-                tokenRequest.setPhone(mobileNumber.getText().toString().trim());
-                tokenRequest.setStateId("1");
-                tokenRequest.setCityId(""+cities.get(spinnerCity.getSelectedIndex()-1).getId());
-                tokenRequest.setLocalityId(""+localities.get(spinnerLocality.getSelectedIndex()-1).getId());
-                tokenRequest.setDob(dob.getText().toString().trim());
-                tokenRequest.setAdharCard(aadhar.getText().toString().trim());
-                tokenRequest.setIsLabour("0");
-
-                Log.d("Single", tokenRequest.getPhone());*/
-
-                //SignUp Function Call
-                String fullname = fullName.getText().toString().trim();
-                boolean isvalidName = new Validator().validInput(fullname);
-                Log.d(TAG,""+isvalidName+fullname);
-                String pass = password.getText().toString().trim();
-                boolean isValidPassword = new Validator().validInput(pass);
-                String phone = mobileNumber.getText().toString().trim();
-                boolean isValidPhone = new Validator().isValidPhone(phone);
-                String stateId = "1";
-                String dateOfBirth = dob.getText().toString().trim();
-                boolean isValidDob = new Validator().validInput(dateOfBirth);
-                String adhar = aadhar.getText().toString().trim();
-                boolean isValidAdhar = new Validator().isvalidAadhar(adhar);
-                String isLabour = "0";
-                boolean isValidLabour = new Validator().validInput(isLabour);
-
-                if(!isvalidName){
-                    Toast.makeText(getApplicationContext(),"Please Enter Name",Toast.LENGTH_SHORT).show();
-                }else if(!isValidPassword){
-                    Toast.makeText(getApplicationContext(),"Please Enter a Valid Password",Toast.LENGTH_SHORT).show();
-                }else if(!isValidPhone){
-                    Toast.makeText(getApplicationContext(),"Please Enter a valid Phone Number",Toast.LENGTH_SHORT).show();
-                }else if(!isValidDob){
-                    Toast.makeText(getApplicationContext(),"Please Enter your Date of Birth",Toast.LENGTH_SHORT).show();
-                }else if(!isValidAdhar){
-                    Toast.makeText(getApplicationContext(),"Please Enter a valid Aadhar Number",Toast.LENGTH_SHORT).show();
-                }else if(!isValidLabour){
-                    Toast.makeText(getApplicationContext(),"Please Choose! Are You a mason or labour",Toast.LENGTH_SHORT).show();
-                }else {
-                    try {
-                        String cityId = ""+cities.get(spinnerCity.getSelectedIndex()-1).getId();
-                        String localityId = ""+localities.get(spinnerLocality.getSelectedIndex()-1).getId();
-                        producerSignUp(fullname,pass,phone,stateId,cityId,localityId,dateOfBirth,adhar,isLabour);
-                    }catch (Exception e){
-                        Toast.makeText(getApplicationContext(),"Please Choose City and Locality",Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
+        signUpButton.setOnClickListener(signUpButtonListener);
 
 
     }
 
 
-    //SignUp Function
+    /**
+     * A function to SignUp Producer using Retrofit 2.0 Api
+     *
+     * @param fullName   Full name of Producer
+     * @param password   Password of Producer
+     * @param phone      Mobile Number of Producer
+     * @param stateId    State Id of Producer
+     * @param cityId     City Id of Producer
+     * @param localityId Locality id of Producer
+     * @param dob        Date of Birth of Producer
+     * @param adhar      Aadhar Number of Producer
+     * @param isLabour   Designation of Producer
+     *
+     * @throws ArrayIndexOutOfBoundsException While getting cityId and localityId
+     */
 
     private void producerSignUp(String fullName, String password, String phone,
                                 String stateId, String cityId, String localityId,
-                                String dob, String adhar, String isLabour)
-    {
-        Call<TokenResponse> producerSignUp = apiInterface.signUpProducer(fullName,password,phone,
-                stateId,cityId,localityId,dob,adhar,isLabour);
-        Log.d("Single","Log Error ");
+                                String dob, String adhar, String isLabour) {
+        Call<TokenResponse> producerSignUp = apiInterface.signUpProducer(fullName, password, phone,
+                stateId, cityId, localityId, dob, adhar, isLabour);
+        Log.d("Single", "Log Error ");
 
         producerSignUp.enqueue(new Callback<TokenResponse>() {
             @Override
             public void onResponse(Call<TokenResponse> call, Response<TokenResponse> response) {
-                if(response.code()==200){
-                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                if (response.code() == 200) {
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                     TokenResponse response1 = response.body();
-                    Log.d("Single",""+response1.getName()+response1.getStatus());
+                    Log.d("Single", "" + response1.getName() + response1.getStatus());
                 }
             }
 
             @Override
             public void onFailure(Call<TokenResponse> call, Throwable t) {
-                Log.d("Single","Some Error");
+                Log.d("Single", "Some Error");
             }
         });
     }
 
-    //GET Cities Function
+    /**
+     * A function to get available cities from the database for spinners data.
+     *
+     * @throws ArrayIndexOutOfBoundsException While getting cityId
+     */
 
-    private void getCities()
-    {
+    private void getCities() {
         final Call<List<Location>> city = apiInterface.getCities();
         city.enqueue(new Callback<List<Location>>() {
             @Override
@@ -259,7 +201,7 @@ public class VendorSignupActivity extends AppCompatActivity {
                 citySize = cities.size();
                 cityName.add("Choose City");
                 for (int i = 1; i <= citySize; i++) {
-                    cityName.add(cities.get(i-1).getName());
+                    cityName.add(cities.get(i - 1).getName());
                 }
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_item, cityName);
                 spinnerCity.setAdapter(adapter);
@@ -272,20 +214,23 @@ public class VendorSignupActivity extends AppCompatActivity {
         });
     }
 
-    //GET Localities Function
+    /**
+     * A function to get available Localities from the database for spinners data.
+     *
+     * @throws ArrayIndexOutOfBoundsException While getting localityId
+     */
 
-    private void getLocalities()
-    {
+    private void getLocalities() {
         final Call<List<Location>> locality = apiInterface.getLocalities();
         locality.enqueue(new Callback<List<Location>>() {
             @Override
             public void onResponse(Call<List<Location>> call, Response<List<Location>> response) {
                 localities = response.body();
                 localitySize = localities.size();
-                Log.d("Single",""+localities.size());
+                Log.d("Single", "" + localities.size());
                 LocalitiesName.add("Choose Locality");
                 for (int i = 1; i <= localitySize; i++) {
-                    LocalitiesName.add(localities.get(i-1).getName());
+                    LocalitiesName.add(localities.get(i - 1).getName());
                     Log.d("Localities", LocalitiesName.get(i));
                 }
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_item, LocalitiesName);
@@ -298,4 +243,72 @@ public class VendorSignupActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    //On Click Listener for signUp Button
+    View.OnClickListener signUpButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String fullname = fullName.getText().toString().trim();
+            boolean isvalidName = new Validator().validInput(fullname);
+            Log.d(TAG, "" + isvalidName + fullname);
+            String pass = password.getText().toString().trim();
+            boolean isValidPassword = new Validator().validInput(pass);
+            String phone = mobileNumber.getText().toString().trim();
+            boolean isValidPhone = new Validator().isValidPhone(phone);
+            String stateId = "1";
+            String dateOfBirth = dob.getText().toString().trim();
+            boolean isValidDob = new Validator().validInput(dateOfBirth);
+            String adhar = aadhar.getText().toString().trim();
+            boolean isValidAdhar = new Validator().isvalidAadhar(adhar);
+            String isLabour = "0";
+            boolean isValidLabour = new Validator().validInput(isLabour);
+
+            if (!isvalidName) {
+                Toast.makeText(getApplicationContext(), "Please Enter Name", Toast.LENGTH_SHORT).show();
+            } else if (!isValidPassword) {
+                Toast.makeText(getApplicationContext(), "Please Enter a Valid Password", Toast.LENGTH_SHORT).show();
+            } else if (!isValidPhone) {
+                Toast.makeText(getApplicationContext(), "Please Enter a valid Phone Number", Toast.LENGTH_SHORT).show();
+            } else if (!isValidDob) {
+                Toast.makeText(getApplicationContext(), "Please Enter your Date of Birth", Toast.LENGTH_SHORT).show();
+            } else if (!isValidAdhar) {
+                Toast.makeText(getApplicationContext(), "Please Enter a valid Aadhar Number", Toast.LENGTH_SHORT).show();
+            } else if (!isValidLabour) {
+                Toast.makeText(getApplicationContext(), "Please Choose! Are You a mason or labour", Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    String cityId = "" + cities.get(spinnerCity.getSelectedIndex() - 1).getId();
+                    String localityId = "" + localities.get(spinnerLocality.getSelectedIndex() - 1).getId();
+                    producerSignUp(fullname, pass, phone, stateId, cityId, localityId, dateOfBirth, adhar, isLabour);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Please Choose City and Locality", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
+
+    View.OnClickListener dateOfBirthListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            Log.d("Single", "" + spinnerLocality.getSelectedIndex());
+
+            DatePickerDialog dialog = new DatePickerDialog(
+                    VendorSignupActivity.this,
+                    android.R.style.Theme_Material_Light_Dialog_NoActionBar_MinWidth,
+                    onDateSetListener,
+                    year, month, day
+            );
+
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+            dialog.show();
+        }
+    };
+
+
 }
